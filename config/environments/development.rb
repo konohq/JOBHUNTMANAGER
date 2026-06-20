ab7449f84b554e0bae92c6db51245441ea059aee
kono <aporokun114514@gmail.com>
2026-06-20 17:18:54 +0900

    fix: GitHub ActionsのCI失敗を修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,10 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = {
-  host: 'localhost',
-  port: 3000
-}
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
